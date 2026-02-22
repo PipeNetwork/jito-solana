67326133c0dcc913525028b9dfc07add2ff58222
David Rhodus <david.rhodus859@gmail.com>
2026-02-22 12:36:07 -0800

    fix build after fair removal
    (cherry picked from commit 29eb7e2b66bbfbeddf3d02253d4cf97ed78f088a)
    (cherry picked from commit 3c3cf2d2eb4fc3666edfe868317c32450656ef4f)
    (cherry picked from commit d88ae3946792012565e8e1757f10e2257a2bd82c)
    (cherry picked from commit 4b711ee579f4cf864941c1e0ff60fdc4380de562)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -379,7 +379,6 @@ pub struct ValidatorConfig {
     pub wait_to_vote_slot: Option<Slot>,
     pub runtime_config: RuntimeConfig,
     pub banking_trace_dir_byte_limit: banking_trace::DirByteLimit,
-    pub tx_io_check: Option<String>,
     pub block_verification_method: BlockVerificationMethod,
     pub block_production_method: BlockProductionMethod,
     pub block_production_num_workers: NonZeroUsize,
@@ -407,7 +406,6 @@ pub struct ValidatorConfig {
     pub tip_manager_config: TipManagerConfig,
     pub bam_url: Arc<ArcSwap<Option<String>>>,
     pub solanacdn: Option<crate::solanacdn::SolanaCdnConfig>,
-    pub fast_shreds: Option<solana_turbine::broadcast_stage::FastShredsConfig>,
 }
 
 impl ValidatorConfig {
@@ -467,7 +465,6 @@ impl ValidatorConfig {
             wait_to_vote_slot: None,
             runtime_config: RuntimeConfig::default(),
             banking_trace_dir_byte_limit: 0,
-            tx_io_check: None,
             block_verification_method: BlockVerificationMethod::default(),
             block_production_method: BlockProductionMethod::default(),
             block_production_num_workers: BankingStage::default_num_workers(),
@@ -500,7 +497,6 @@ impl ValidatorConfig {
             tip_manager_config: TipManagerConfig::default(),
             bam_url: Arc::new(ArcSwap::from_pointee(None)),
             solanacdn: None,
-            fast_shreds: None,
         }
     }
 
@@ -1798,7 +1794,6 @@ impl Validator {
             entry_notification_sender,
             blockstore.clone(),
             &config.broadcast_stage_type,
-            config.fast_shreds.clone(),
             xdp_sender,
             exit.clone(),
             node.info.shred_version(),
